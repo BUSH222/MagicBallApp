2fe87c721dd926e7ea112ce00cda454e022217ca
Ted V <58144503+BUSH222@users.noreply.github.com>
2023-10-11 20:09:39 +0300

    Things

--- MagicBallApp/ViewController.swift
+++ MagicBallApp/ViewController.swift
@@ -8,5 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ballPicture: UIImageView!
+    
     var ballArray = [UIImage(imageLiteralResourceName: "ball1"), UIImage(imageLiteralResourceName: "ball2"), UIImage(imageLiteralResourceName: "ball3"), UIImage(imageLiteralResourceName: "ball4"), UIImage(imageLiteralResourceName: "ball5")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    func changeImage() {
+        ballPicture.image = ballArray.randomElement()
+    }
+    
+    @IBAction func BoringButton(_ sender: UIButton) {changeImage()}
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?){
+        if motion == .motionShake { // Shake detection stuff for fun
+            changeImage()
+        }
+    }
 }
